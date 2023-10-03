c4129c26d960f8f7133d2bb3cafc32fbb5755430
daniel endaylalu <endaylaludaniel@gmail.com>
2023-10-03 21:35:32 +0300

    added nav-bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 const App = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <div>
-      <nav>
-        <h3>Dan.stack</h3>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
+      <nav className="navbar">
+        <h3 className="logo">
+          <a href="#">
+            Dan.<span>stack</span>
+          </a>
+        </h3>
+        {!isNavOpen ? (
+          <FontAwesomeIcon
+            icon={faBars}
+            className="nav-icon hum"
+            onClick={() => {
+              setIsNavOpen(!isNavOpen);
+            }}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faXmark}
+            className="nav-icon"
+            onClick={() => {
+              setIsNavOpen(!isNavOpen);
+            }}
+          />
+        )}
+        <ul className={`nav ${isNavOpen && "min-nav"}`}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </nav>
     </div>
